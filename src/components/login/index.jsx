3226c79629439e1f02a87a4a54568ab406e69aa8
meixuan <754558592@qq.com>
2018-12-03 20:27:25 +0800

    注册登录页面完成

--- src/components/login/index.jsx
+++ src/components/login/index.jsx
@@ -1,12 +1,46 @@
 /*
- 用户登陆的路由组件
+ 用户注册的路由组件
  */
 import React, {Component} from 'react'
+import { NavBar,Button,WingBlank, WhiteSpace,List,InputItem } from 'antd-mobile';
+import Logo from '../logo/logo';
 
-export default class Login extends Component {
-  render () {
+class Login extends Component {
+
+  handleChange=(type,value)=>{
+    this.setState({
+      [type]:value
+    })
+  }
+  register = ()=>{
+    this.props.history.replace('/register');
+  }
+  gologin= () =>{
+    this.props.history.replace('/login');
+  }
+
+  render() {
+
     return (
-      <div>login</div>
+      <div>
+        <NavBar>硅谷直聘</NavBar>
+        <Logo/>
+        <WingBlank>
+          <List>
+            <InputItem onChange={val=>{this.handleChange('username',val)}}>用户名:</InputItem>
+            <WhiteSpace/>
+            <InputItem onChange={val=>{this.handleChange('password',val)}}>密&nbsp;&nbsp;&nbsp;码:</InputItem>
+            <WhiteSpace/>
+          </List>
+          <WhiteSpace/>
+          <Button type="primary" onClick={this.gologin}>登录</Button>
+          <WhiteSpace/>
+          <Button onClick={this.register}>还没有账户</Button>
+        </WingBlank>
+      </div>
+
+
     )
   }
 }
+export default Login;
--- src/components/register/index.jsx
+++ src/components/register/index.jsx
@@ -2,11 +2,59 @@
  用户注册的路由组件
  */
 import React, {Component} from 'react'
+import { NavBar,Button,WingBlank, WhiteSpace,List,InputItem,Radio } from 'antd-mobile';
+import Logo from '../logo/logo';
+const Item = List.Item;
+class Register extends Component {
+  state = {
+    // isBossChecked: true,
+    laoban: true,
+    username: '',
+    password: '',
+    rePassword: ''
+  }
+handleChange=(type,value)=>{
+  this.setState({
+    [type]:value
+  })
+}
+register = ()=>{
+    const {username,password,rePassword,laoban} = this.state;
+    console.log(username,password,rePassword,laoban);
+    }
+gologin= () =>{
+  this.props.history.replace('/login');
+}
 
-export default class Register extends Component {
   render() {
+    const {laoban} = this.state;
     return (
-      <div>Register</div>
+      <div>
+        <NavBar>硅谷直聘</NavBar>
+        <Logo/>
+        <WingBlank>
+        <List>
+          <InputItem onChange={val=>{this.handleChange('username',val)}}>用户名:</InputItem>
+          <WhiteSpace/>
+          <InputItem onChange={val=>{this.handleChange('password',val)}}>密&nbsp;&nbsp;&nbsp;码:</InputItem>
+          <WhiteSpace/>
+          <InputItem onChange={val=>{this.handleChange('rePassword',val)}}>确认密码:</InputItem>
+          <WhiteSpace/>
+        </List>
+          <Item>
+            用户类型:&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            <Radio checked ={!laoban} onChange={this.handleChange.bind(null,'laoban',false)}>大神</Radio>
+            <Radio checked ={laoban} onChange={this.handleChange.bind(null,'laoban',true)}>老板</Radio>
+          </Item>
+          <WhiteSpace/>
+          <Button type="primary" onClick={this.register}>注册</Button>
+          <WhiteSpace/>
+          <Button onClick={this.gologin}>已有账户</Button>
+        </WingBlank>
+      </div>
+
+
     )
   }
 }
+export default Register;
